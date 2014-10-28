3ce1f0683848e23e4a12360b730db8908f1aeb71
Ricardo Barros <ricardofbarros@hotmail.com>
2014-10-28 20:59:40 +0000

    bump version 0.1.3

--- app/templates/lib/factory/_api.js
+++ app/templates/lib/factory/_api.js
@@ -5,7 +5,8 @@ function Factory() {
   var config = require('config');
   var Api = require('muzzley-idk').helpers.Api;
 
-  return new Api(config.muzzley.api.credentials.id, config.muzzley.api.credentials.key);
+  return new Api(config.muzzley.api.credentials.id, config.muzzley.api.credentials.key, 
+    {url: config.muzzley.api.url}); 
 }
 
 module.exports = Factory;
