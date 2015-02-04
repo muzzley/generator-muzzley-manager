f43751406883952abe599de288054b92808cdc3c
Ricardo Barros <ricardofbarros@hotmail.com>
2015-02-04 15:18:39 +0000

    new default subscription handler, improve how we handle unsubs

--- app/templates/lib/plugins/subscriptions/_handlers.js
+++ app/templates/lib/plugins/subscriptions/_handlers.js
@@ -1,74 +1,70 @@
-// Dependencies /////////////////////////////////////////
-var config = require('config');
+// Dependencies
 var log = require('lib/factory/log')();
-var api = require('lib/factory/api')();
 var async = require('async');
 var Subscription = require('lib/models/Subscription');
-
-// Hapi Helpers
+var Channel = require('lib/models/Channel');
 var Boom = require('boom');
-var Joi = require('joi');
-
-// Your dependecies goes down here
-
-/////////////////////////////////////////////////////////
-
 
 var routes = {};
 
 routes.subscribe = function (request, reply) {
   var muzzleyId = request.query.user;
-  
+
   async.each(request.payload.channels, function (channel, cb) {
 
     Subscription.get(muzzleyId, channel.id, function(err, subscription) {
-      if(err) return cb(err);
+      if(err) {
+        return cb(err);
+      }
 
-      if(subscription.status == channel.status) {
+      // Handle if user is
+      // unsubscribing this channel
+      if(channel.status === 'off') {
+        log.debug({channel: channel.id, username: request.query.user}, 'Unsubscribing');
+
+        // First we will get all
+        // subscriptions from this
+        // device
+        return Subscription.getAllFromDevice(channel.id, function(err, subs) {
+          if(err) {
+            return cb(err);
+          }
+
+          // Delete the subscription
+          // the user requested to
+          Subscription.del(muzzleyId, channel.id, function(err) {
+            if(err) {
+              return cb(err);
+            }
+
+            // Check if all subscriptions returned from this channel
+            // if they'r are lower or equal to 1
+            // if yes delete the Channel
+            // since no one is subscribed to it, therefore nobody is using
+            // this information
+            if(subs.length <= 1) {
+              Channel.del(channel.id, function(err) {
+                return cb(err);
+              });
+            } else {
+              return cb();
+            }
+          });
+        });
+      } else {
         log.debug({channel: channel.id, username: request.query.user}, 'No changes on this subscription');
         return cb();
       }
-
-      if(subscription.status == 'off' && channel.status == 'on') {
-        log.debug({channel: channel.id, username: request.query.user}, 'New subscription');
-      }
-      else if(subscription.status == 'on' && channel.status == 'off') {
-        log.debug({channel: channel.id, username: request.query.user}, 'Unsubscribing');
-      }
-
-      subscription.status = channel.status;
-      
-      // Post to api the subscription if the user is making
-      // a new subscriptions
-      // If the user is unsubscribing
-      // save the subscription as status off
-      if(channel.status == 'on') {
-        return postSubscription(channel.id, muzzleyId, cb);
-      } else {
-        return subscription.save(cb); 
-      }
     });
   }, function (err) {
-    if (err) log.warn(err, 'Failed to subscribe');
+    if (err) {
+      log.error(err);
+      return reply(Boom.badRequest(err));
+    }
 
-    reply(200);
+    return reply(200);
   });
 };
 
-
-function postSubscription(channelId, muzzleyId, cb) {
-  api.post(channelId, [{
-    content: ' ',
-    photoUrl: config.server.host+config.server.images.firstPost
-  }], function (err) {
-    if (err) {
-      log.error('{{ lname|capitalize }} post to api failed', err);
-      return cb(err);
-    }
-    
-    log.info({channel: channelId, muzzleyProfile: muzzleyId, photoUrl: config.server.host+config.server.images.firstPost}, '{{ lname|capitalize }} first post');
-    return cb();
-  });
-}
 
 module.exports = routes;
