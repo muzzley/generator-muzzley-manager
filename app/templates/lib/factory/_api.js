a93d3bf01ca6709fcc3d2edff35f351c493fed3c
Ricardo Barros <ricardofbarros@hotmail.com>
2015-05-11 11:21:18 +0100

    update api factory

--- app/templates/lib/factory/_api.js
+++ app/templates/lib/factory/_api.js
@@ -1,13 +1,15 @@
 // Dependencies
 var config = require('config');
+var storage = require('./storage');
 var Api = require('muzzley-idk').helpers.Api;
 
 
 var api = new Api(
-  config.muzzley.api.credentials.id,
-  config.muzzley.api.credentials.key,
+  config.muzzley.app.profile,
+  config.muzzley.api.credential,
   {
-    url: config.muzzley.api.url
+    url: config.muzzley.api.url,
+    storage: storage
   }
 );
 
