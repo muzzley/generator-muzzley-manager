502f17652276b99f6b9ded6367bcc93ec8711954
Ricardo Barros <ricardofbarros@hotmail.com>
2015-05-11 11:23:17 +0100

    add expire and persist to subscription model

--- app/templates/lib/models/_Subscription.js
+++ app/templates/lib/models/_Subscription.js
@@ -6,20 +6,29 @@ var Subscription = Model.extend({
   props: {
     muzzleyId: 'string',
     channelId: 'string',
-    providerId: 'string',
-    alias: 'string'
+    providerId: 'string'
   },
   keys: ['muzzleyId', 'channelId']
 });
 
-
-Subscription.getAllFromDevice = function(channelId, cb) {
+Subscription.getAllFromDevice = function (channelId, cb) {
   return Subscription.mget({muzzleyId: '*', channelId: channelId}, cb);
 };
 
-Subscription.getAllFromUser = function(userId, cb) {
+Subscription.getAllFromUser = function (userId, cb) {
   return Subscription.mget({muzzleyId: userId, channelId: '*'}, cb);
 };
 
+Subscription.expire = function (keyObj, secs, cb) {
+  var key = this.key(keyObj);
+
+  return this.storage.client().expire(key, secs, cb);
+};
+
+Subscription.persist = function (keyObj, cb) {
+  var key = this.key(keyObj);
+
+  return this.storage.client().persist(key, cb);
+};
 
 module.exports = Subscription;
